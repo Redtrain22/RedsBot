7a6905857f3ff6177dbabe046de2ae88f6d66dde
Redtrain22 <Redtrain22@users.noreply.github.com>
2021-10-21 17:03:13 -0400

    Converting the interactionCreate event to typescript.

--- bot/events/interactionCreate.ts
+++ bot/events/interactionCreate.ts
@@ -1,14 +1,16 @@
-const { Statistic } = require("../managers/Database.js");
-const commandManager = require("../managers/Commands.js");
-const commands = commandManager.getCommands();
-const logger = require("../managers/Logger.js");
+// import { Statistic } from "../managers/Database";
+import { getCommands } from "../managers/Commands";
+const commands = getCommands();
+import * as logger from "../managers/Logger";
+import { Client, Interaction } from "discord.js";
+import { Statistic } from "../managers/Database";
 
-module.exports = async (client, interaction) => {
+export const run = async (client: Client, interaction: Interaction): Promise<void> => {
 	// Fetch or create our statistic
 	// The reason statistic is wrapped in [] is because findOrCreate actually outputs 2 variables and we only need the first.
 	const [statistic] = await Statistic.findOrCreate({
-		where: { guildId: interaction.guild.id },
-		defaults: { guildId: interaction.guild.id },
+		where: { guildId: interaction.guild?.id },
+		defaults: { guildId: interaction.guild?.id as string },
 	});
 
 	// Add one to our statistic
@@ -21,10 +23,10 @@ module.exports = async (client, interaction) => {
 			if (!commands.get(interaction.commandName)) return;
 
 			// Run the command.
-			await commands.get(interaction.commandName).run(client, interaction);
+			await commands.get(interaction.commandName)?.run(client, interaction);
 
 			logger.log(
-				`"${interaction.member.user.username}#${interaction.member.user.discriminator}" ran command ${interaction.commandName} in guild "${interaction.guild.name}" (${interaction.guild.id})`,
+				`"${interaction.member?.user.username}#${interaction.member?.user.discriminator}" ran command ${interaction.commandName} in guild "${interaction.guild?.name}" (${interaction.guild?.id})`,
 				"cmd"
 			);
 		} catch (error) {
