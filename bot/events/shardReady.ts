cf86a5ea4ba8a666e36f3296ab3ccb3fd03b3510
Redtrain22 <Redtrain22@users.noreply.github.com>
2021-10-22 16:16:55 -0400

    Converting the shardReady event.

---
+++ bot/events/shardReady.ts
@@ -0,0 +1,14 @@
+import { Client } from "discord.js";
+import { log } from "../managers/Logger";
+
+export const run = (client: Client, shardID: number): void => {
+	log(
+		`Shard ${shardID} Ready: ${client.user?.tag}, now serving ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} users in ${
+			client.guilds.cache.size
+		} servers.`,
+		"ready"
+	);
+
+	// Make the bot "play the game" which is the help command with default prefix.
+	client.user?.setActivity("/help (me)", { type: "PLAYING" });
+};
