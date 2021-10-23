08333a075f92d4d1805c5280c1c165a0dfc8ef8a
Redtrain22 <Redtrain22@users.noreply.github.com>
2021-10-23 03:47:03 -0400

    Modifying the Event type for non-async events.

--- bot/types/Event.ts
+++ bot/types/Event.ts
@@ -11,5 +11,5 @@ export interface Event extends EventOptions {
 
 	// Any is used because we don't know what kind of parameters or return type the event will have FOR SURE.
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	run: (client: Client, ...args: any[]) => Promise<void>;
+	run: (client: Client, ...args: any[]) => Promise<void> | void;
 }
