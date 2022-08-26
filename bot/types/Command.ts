1331db15be893c3bacd16be727afa2ce0d88b6d3
Redtrain22 <Redtrain22@users.noreply.github.com>
2022-08-26 02:27:41 -0400

    docs: update type for defaultPermission and make a comment on why the solution is what it is

--- bot/types/Command.ts
+++ bot/types/Command.ts
@@ -8,7 +8,12 @@ export interface Command {
 		enabled: boolean;
 		guildOnly: boolean;
 		description: string;
-		defaultPermission: typeof PermissionFlagsBits;
+		// TS doesn't support bigints in enums, but since discord permissions are bigints we need them used here.
+		// https://github.com/discordjs/discord-api-types/issues/483
+		// https://github.com/microsoft/TypeScript/issues/37783
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		defaultPermission: PermissionFlagsBits;
 		options: SlashCommandBuilder;
 	};
 }
