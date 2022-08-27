513d44a16ae56f9504bf45265b81bda62a218f23
Redtrain22 <Redtrain22@users.noreply.github.com>
2022-08-27 16:02:20 -0400

    refactor(commands/skip): update to new structure and discord.js v14

--- bot/commands/skip.ts
+++ bot/commands/skip.ts
@@ -1,40 +1,46 @@
 import * as queueManager from "../managers/Queue.js";
 import * as playerManager from "../managers/Player.js";
-import { Client, CommandInteraction } from "discord.js";
+import { Client, ChatInputCommandInteraction, SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 
-async function run(client: Client, interaction: CommandInteraction): Promise<void> {
+async function run(client: Client, interaction: ChatInputCommandInteraction): Promise<void> {
 	const songNumber = interaction.options.getInteger("song");
-	if (interaction.guild == null) return await interaction.reply({ content: "Please run this command from a guild." });
+	if (interaction.guild == null) {
+		await interaction.reply({ content: "Please run this command from a guild." });
+		return;
+	}
 
 	if (songNumber != null && songNumber > 0 && songNumber <= Number(queueManager.getQueue(interaction.guild.id)?.length)) {
 		const skippedSong = queueManager.getQueue(interaction.guild.id)?.splice(songNumber - 1, 1)[0];
 
-		return await interaction.reply({ content: `Skipped song number ${songNumber}, which was ${skippedSong?.metadata.youtubeURL}` });
+		await interaction.reply({ content: `Skipped song number ${songNumber}, which was ${skippedSong?.metadata.youtubeURL}` });
+		return;
 	} else if (songNumber == null || songNumber == 0) {
 		const currentSong = queueManager.getCurrentSong(interaction.guild.id);
 		playerManager.getPlayer(interaction.guild.id)?.stop();
-		return await interaction.reply({ content: `Skipped currently playing song. The current song was ${currentSong?.metadata.youtubeURL}` });
+		await interaction.reply({ content: `Skipped currently playing song. The current song was ${currentSong?.metadata.youtubeURL}` });
+		return;
 	}
 }
 
-const help = {
-	name: "skip",
-	description: "Skip the current song, or a song in the queue.",
-	options: [
-		{
-			type: "INTEGER",
-			name: "song",
-			description: "Skip a song in the queue.",
-			required: false,
-		},
-	],
-	aliases: [],
-	level: "User",
-};
+const name = "skip";
+const enabled = true;
+const guildOnly = true;
+const description = "Skip the current song, or a song in the queue.";
+const defaultPermission = PermissionFlagsBits.UseApplicationCommands;
+const options = new SlashCommandBuilder()
+	.setName(name)
+	.setDescription(description)
+	.addStringOption((option) => option.setName("song").setDescription("Skip a song in queue.").setRequired(false))
+	.setDMPermission(!guildOnly)
+	.setDefaultMemberPermissions(defaultPermission);
 
 const config = {
-	enabled: true,
-	guildOnly: true,
+	name,
+	enabled,
+	guildOnly,
+	description,
+	defaultPermission,
+	options,
 };
 
-export { run, help, config };
+export { run, config };
