82f46c5dc18ad607e592c928c536ef4f13ad8cd6
Redtrain22 <Redtrain22@users.noreply.github.com>
2023-01-08 11:33:43 -0500

    refactor(events/interactionCreate): switch the type of interaction, use handle functions for the specific type

--- bot/events/interactionCreate.ts
+++ bot/events/interactionCreate.ts
@@ -1,20 +1,14 @@
 import { getCommands } from "../managers/Commands.js";
 const commands = getCommands();
 import * as logger from "../managers/Logger.js";
-import { Client, Interaction, InteractionType } from "discord.js";
+import { ChatInputCommandInteraction, Client, Interaction, InteractionType } from "discord.js";
 import { Statistic } from "../managers/Database.js";
 
 const once = false;
 const name = "interactionCreate";
 
 const run = async (client: Client, interaction: Interaction): Promise<void> => {
-	let guildId = null;
-
-	if (interaction.guild == null) {
-		guildId = "0";
-	} else {
-		guildId = interaction.guild.id;
-	}
+	const guildId = interaction?.guild?.id ?? "0";
 
 	// Fetch or create our statistic
 	// The reason statistic is wrapped in [] is because findOrCreate actually outputs 2 variables and we only need the first.
@@ -26,57 +20,64 @@ const run = async (client: Client, interaction: Interaction): Promise<void> => {
 	// Add one to our statistic
 	statistic.add();
 
-	// Check if the interaction is a command or not.
-	if (interaction.type == InteractionType.ApplicationCommand) {
-		try {
-			// Return if it's not a command.
-			if (!commands.get(interaction.commandName)) return;
+	// The bot can recieve multiple types of interactions, we switch them then run a handle function.
+	// The handle functions exist to break up giant case statements.
+	switch (interaction.type) {
+		case InteractionType.ApplicationCommand:
+			if (!interaction.isChatInputCommand()) return;
+			await handleCommand(client, interaction);
 
-			// Run the command.
-			const cmd = commands.get(interaction.commandName);
-
-			if (!cmd?.config.enabled) {
-				await interaction.reply({
-					content: `Sorry, but the ${interaction.commandName} command is currently disabled. It will be back sometime soon:tm:`,
-				});
-				return;
-			}
-
-			if (cmd.config.guildOnly && !interaction.guild) {
-				await interaction.reply({
-					content: `Sorry, but the ${interaction.commandName} command can only be run inside a guild. Please run this in a Discord server`,
-				});
-				return;
-			}
-
-			await cmd.run(client, interaction);
-
-			if (!interaction.guild) {
-				logger.log(
-					`"${interaction.member?.user.username}#${interaction.member?.user.discriminator}" ran command ${interaction.commandName} in DMs`,
-					"cmd"
-				);
-			} else {
-				logger.log(
-					`"${interaction.member?.user.username}#${interaction.member?.user.discriminator}" ran command ${interaction.commandName} in guild "${interaction.guild?.name}" (${interaction.guild?.id})`,
-					"cmd"
-				);
-			}
-		} catch (error) {
-			if (interaction.deferred || interaction.replied) {
-				await interaction.followUp({
-					content: "There was an error executing this, please try again later or contact the bot owner.",
-					ephemeral: true,
-				});
-			} else {
-				await interaction.reply({
-					content: "There was an error executing this, please try again later or contact the bot owner.",
-					ephemeral: true,
-				});
-			}
-			logger.error(error);
-		}
+			break;
 	}
 };
+
+async function handleCommand(client: Client, interaction: ChatInputCommandInteraction) {
+	try {
+		// Run the command.
+		const cmd = commands.get(interaction.commandName);
+		if (!cmd) return;
+
+		if (!cmd.config.enabled) {
+			await interaction.reply({
+				content: `Sorry, but the ${interaction.commandName} command is currently disabled. It will be back sometime soon:tm:`,
+			});
+			return;
+		}
+
+		if (cmd.config.guildOnly && !interaction.guild) {
+			await interaction.reply({
+				content: `Sorry, but the ${interaction.commandName} command can only be run inside a guild. Please run this in a Discord server`,
+			});
+			return;
+		}
+
+		await cmd.run(client, interaction);
+
+		if (!interaction.guild) {
+			logger.log(
+				`"${interaction.member?.user.username}#${interaction.member?.user.discriminator}" ran command ${interaction.commandName} in DMs`,
+				"cmd"
+			);
+		} else {
+			logger.log(
+				`"${interaction.member?.user.username}#${interaction.member?.user.discriminator}" ran command ${interaction.commandName} in guild "${interaction.guild?.name}" (${interaction.guild?.id})`,
+				"cmd"
+			);
+		}
+	} catch (error) {
+		if (interaction.deferred || interaction.replied) {
+			await interaction.followUp({
+				content: "There was an error executing this, please try again later or contact the bot owner.",
+				ephemeral: true,
+			});
+		} else {
+			await interaction.reply({
+				content: "There was an error executing this, please try again later or contact the bot owner.",
+				ephemeral: true,
+			});
+		}
+		logger.error(error);
+	}
+}
 
 export { once, name, run };
